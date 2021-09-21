0482f7e855252f2c9313b48138ce803e3a3de45c
Konstantin Bogdanov <konstantin-b@yandex-team.ru>
2021-09-21 17:20:58 +0300

    Update default Docker registry name and image version

--- api/v1alpha1/const.go
+++ api/v1alpha1/const.go
@@ -1,9 +1,8 @@
 package v1alpha1
 
 const (
-	//RegistryPath = "cr.yandex/ydb/ydb"
-	RegistryPath = "cr.yandex/crpbo4q9lbgkn85vr1rm/ydb" // fixme
-	DefaultTag   = "21.4.14"
+	RegistryPath = "cr.yandex/yc/ydb"
+	DefaultTag   = "21.4.29"
 
 	ImagePathFormat = "%s:%s"
 
