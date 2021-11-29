0ea7c6ef4851264bfe6b1b060ffd17e5ad64de98
Konstantin Bogdanov <konstantin-b@yandex-team.ru>
2021-11-29 08:29:46 +0300

    Fix init configuration to support gRPC-only installations

--- internal/configuration/templates/init_script_storage.go
+++ internal/configuration/templates/init_script_storage.go
@@ -5,9 +5,17 @@ set -eu
 
 echo DefineBox.txt
 cat /opt/kikimr/cfg/DefineBox.txt
+{{- if .Spec.Service.Interconnect.TLSConfiguration.Enabled }}
+/opt/kikimr/bin/kikimr -s grpcs://{{ .Name }}-grpc.{{ .Namespace }}.svc.cluster.local:2135 admin bs config invoke --proto-file /opt/kikimr/cfg/DefineBox.txt
+{{- else }}
 /opt/kikimr/bin/kikimr admin bs config invoke --proto-file /opt/kikimr/cfg/DefineBox.txt
+{{- end }}
 
 echo DefineStoragePools.txt
 cat /opt/kikimr/cfg/DefineStoragePools.txt
+{{- if .Spec.Service.Interconnect.TLSConfiguration.Enabled }}
+/opt/kikimr/bin/kikimr -s grpcs://{{ .Name }}-grpc.{{ .Namespace }}.svc.cluster.local:2135 admin bs config invoke --proto-file /opt/kikimr/cfg/DefineStoragePools.txt
+{{- else }}
 /opt/kikimr/bin/kikimr admin bs config invoke --proto-file /opt/kikimr/cfg/DefineStoragePools.txt
+{{- end }}
 `
