e45984dc6d46e0642bae38d52ccae69ea27ba57a
Mikhail Babich <mbabich@yandex-team.ru>
2022-03-04 22:39:01 +0300

    Fix storage init command

--- internal/controllers/storage/init.go
+++ internal/controllers/storage/init.go
@@ -50,17 +50,20 @@ func (r *StorageReconciler) runInitScripts(ctx context.Context, storage *resourc
 	if !meta.IsStatusConditionTrue(storage.Status.Conditions, InitStorageStepCondition) {
 		cmd := []string{
 			fmt.Sprintf("%s/%s", v1alpha1.BinariesDir, v1alpha1.DaemonBinaryName),
-			"admin", "blobstorage", "config", "init",
-			"--yaml-file",
-			fmt.Sprintf("%s/%s", v1alpha1.ConfigDir, v1alpha1.ConfigFileName),
 		}
-
 		if storage.Spec.Service.GRPC.TLSConfiguration.Enabled {
 			cmd = append(
 				cmd,
 				"-s", storage.GetGRPCEndpointWithProto(),
 			)
 		}
+		cmd = append(
+			cmd,
+			"admin", "blobstorage", "config", "init",
+			"--yaml-file",
+			fmt.Sprintf("%s/%s", v1alpha1.ConfigDir, v1alpha1.ConfigFileName),
+		)
+
 		_, _, err := exec.ExecInPod(r.Scheme, r.Config, storage.Namespace, podName, "ydb-storage", cmd)
 		if err != nil {
 			return Stop, ctrl.Result{RequeueAfter: StorageInitializationRequeueDelay}, err
