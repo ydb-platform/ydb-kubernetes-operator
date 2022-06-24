3c08b27a06a0a9682f5950fb1c3b624b04513615
Mikhail Babich <mbabich@yandex-team.ru>
2022-06-24 12:41:57 +0300

    Set length when creaing map

--- internal/resources/resource.go
+++ internal/resources/resource.go
@@ -102,7 +102,7 @@ func CheckObjectUpdatedIgnoreStatus(current, updated runtime.Object) (bool, erro
 }
 
 func CopyDict(src map[string]string) map[string]string {
-	dst := make(map[string]string)
+	dst := make(map[string]string, len(src))
 	for k, v := range src {
 		dst[k] = v
 	}
