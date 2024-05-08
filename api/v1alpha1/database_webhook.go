a991d6d719d8da194c33c51f164cf9ba48b2d9a6
Aleksei Kobzev <kobzonega@yandex.ru>
2024-05-08 18:51:33 +0200

    move encryption config to args

--- api/v1alpha1/database_webhook.go
+++ api/v1alpha1/database_webhook.go
@@ -141,7 +141,7 @@ func (r *DatabaseDefaulter) Default(ctx context.Context, obj runtime.Object) err
 		database.Spec.StorageEndpoint = storage.GetStorageEndpointWithProto()
 	}
 
-	if database.Spec.Configuration != "" || (database.Spec.Encryption != nil && database.Spec.Encryption.Enabled) {
+	if database.Spec.Configuration != "" {
 		configuration, err := buildConfiguration(storage, database)
 		if err != nil {
 			return err
