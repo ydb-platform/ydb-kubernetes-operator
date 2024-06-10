14fee5016786941706a2b09d650e1aac3187bd56
Aleksei Kobzev <kobzonega@yandex.ru>
2024-06-10 15:27:48 +0200

    fix schema configuration

--- internal/configuration/schema/configuration.go
+++ internal/configuration/schema/configuration.go
@@ -1,8 +1,10 @@
 package schema
 
 type Dynconfig struct {
-	Metadata *Metadata              `yaml:"metadata"`
-	Config   map[string]interface{} `yaml:"config"`
+	Metadata       *Metadata              `yaml:"metadata"`
+	Config         map[string]interface{} `yaml:"config"`
+	AllowedLabels  map[string]interface{} `yaml:"allowed_labels"`
+	SelectorConfig []SelectorConfig       `yaml:"selector_config"`
 }
 
 type Configuration struct {
@@ -11,8 +13,14 @@ type Configuration struct {
 }
 
 type Metadata struct {
-	Kind    string `yaml:"kind,omitempty"`
+	Kind    string `yaml:"kind"`
 	Cluster string `yaml:"cluster"`
 	Version uint64 `yaml:"version"`
 	ID      uint64 `yaml:"id,omitempty"`
+}
+
+type SelectorConfig struct {
+	Description string                 `yaml:"description"`
+	Selector    map[string]interface{} `yaml:"selector"`
+	Config      map[string]interface{} `yaml:"config"`
 }
