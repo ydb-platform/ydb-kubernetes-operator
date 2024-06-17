0b791fa9b8f5bfe03f61362a86215cba2eb451e2
kobzonega <122476665+kobzonega@users.noreply.github.com>
2024-06-17 23:12:50 +0300

    YDBOPS-9608 support dynconfig (#200)

--- internal/configuration/schema/configuration.go
+++ internal/configuration/schema/configuration.go
@@ -1,6 +1,26 @@
 package schema
 
+type Dynconfig struct {
+	Metadata       *Metadata              `yaml:"metadata"`
+	Config         map[string]interface{} `yaml:"config"`
+	AllowedLabels  map[string]interface{} `yaml:"allowed_labels"`
+	SelectorConfig []SelectorConfig       `yaml:"selector_config"`
+}
 type Configuration struct {
-	Hosts     []Host     `yaml:"hosts"`
-	KeyConfig *KeyConfig `yaml:"key_config,omitempty"`
+	DomainsConfig *DomainsConfig `yaml:"domains_config"`
+	Hosts         []Host         `yaml:"hosts,omitempty"`
+	KeyConfig     *KeyConfig     `yaml:"key_config,omitempty"`
+}
+
+type Metadata struct {
+	Kind    string `yaml:"kind"`
+	Cluster string `yaml:"cluster"`
+	Version uint64 `yaml:"version"`
+	ID      uint64 `yaml:"id,omitempty"`
+}
+
+type SelectorConfig struct {
+	Description string                 `yaml:"description"`
+	Selector    map[string]interface{} `yaml:"selector"`
+	Config      map[string]interface{} `yaml:"config"`
 }
