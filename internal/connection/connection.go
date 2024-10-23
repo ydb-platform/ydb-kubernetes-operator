5d766eddd1cb8882a5bbff488b9b40c3569d90fa
Mikhail Babich <mbabich@nebius.com>
2024-10-23 10:39:22 +0200

    Timeout for getting ydb token increased from 1s to 10s

--- internal/connection/connection.go
+++ internal/connection/connection.go
@@ -16,7 +16,7 @@ import (
 )
 
 func Open(ctx context.Context, endpoint string, opts ...ydb.Option) (*ydb.Driver, error) {
-	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
 	db, err := ydb.Open(
